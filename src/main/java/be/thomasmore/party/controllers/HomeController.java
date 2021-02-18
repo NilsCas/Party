91eb964877edd15315b6cbc16e95eb0139ba8ae8
NilsCas <nils.cassiers@hotmail.com>
2021-02-18 13:57:57 +0100

    Party Dynamic Page

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -3,20 +3,34 @@ package be.thomasmore.party.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
 
     private final int mySpecialNumber = 729;
+    private final String appName = "It's Partytime!";
+    private final LocalDateTime time = LocalDateTime.now();
+    private final LocalDateTime deadline = time.plusDays(30);
 
-    @GetMapping("/")
+
+    @GetMapping({"/", "/home"})
     public String home(Model model) {
         model.addAttribute("mySpecialNumber",mySpecialNumber);
+        model.addAttribute("appName",appName);
         return "home";
     }
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        model.addAttribute("mySpecialNumber",mySpecialNumber);
         return "about";
+    }
+    @GetMapping("/pay")
+    public String pay(Model model){
+        time.format(DateTimeFormatter.ofPattern("PatternRegex"));
+        model.addAttribute("tijd",time);
+        return "pay";
     }
 }
 
